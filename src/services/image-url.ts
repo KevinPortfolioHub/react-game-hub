c6ae3a746fea5c031299c720f3683b37cb7d465f
Kevin Obi <michaeljordan4life@gmail.com>
2023-09-04 07:24:38 +0100

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,10 +1,12 @@
+import noImage from "../assets/no-image-placeholder.webp";
+
 const getCroppedImageUrl = (url: string | null) => {
+  if (!url) return noImage;
+
   const target = "media/";
-  if (url) {
-    const index = url.indexOf(target) + target.length;
-    return url.slice(0, index) + "crop/600/400/" + url.slice(index);
-  }
-  return "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png";
+  url;
+  const index = url.indexOf(target) + target.length;
+  return url.slice(0, index) + "crop/600/400/" + url.slice(index);
 };
 
 export default getCroppedImageUrl;
