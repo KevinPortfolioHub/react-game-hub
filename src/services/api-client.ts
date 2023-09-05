29bcb7e96ee687a28a2c8383461d3f48f44e818c
Kevin Obi <michaeljordan4life@gmail.com>
2023-09-05 10:11:13 +0100

    Build update

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
-  params: { key: import.meta.env.VITE_RAWG_API_KEY },
+  params: { key: "ea9972067acb4ae3b5a32c0d0111184c" },
 });
